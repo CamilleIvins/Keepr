0005d955e6760f8c5587d2220c0cbc926261ef99
Camille Ivins <camillemivins@gmail.com>
2023-10-20 17:19:56 -0600

    Just created VaultKeeps repo

---
+++ Keepr/Repositories/VaultKeepsRepository.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keepr.Interfaces;
+
+namespace Keepr.Repositories;
+
+public class VaultKeepsRepository
+{
+    public VaultKeep Create(VaultKeep newData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<VaultKeep> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public VaultKeep GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
