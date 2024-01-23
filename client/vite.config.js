7be44e5c5aabd062888ec519b47a0e762740aae6
Camille Ivins <camillemivins@gmail.com>
2024-01-23 13:50:19 -0700

    Thanks DJ!

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    // outDir: 'docs',
+    outDir: '../Keepr/wwwroot',
     sourcemap: false
   },
   server: {
